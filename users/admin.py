6086e975c2efc95062892b2aa916e54178705810
manyyeon <udayeon@naver.com>
2022-08-15 19:38:32 +0900

    Feat : users models 설계, admin 추가, fake data 추가

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from rooms import models as room_models
 from . import models
+
+
+class RoomInline(admin.TabularInline):
+
+    model = room_models.Room
 
 
 @admin.register(models.User)
 class CustomUserAdmin(UserAdmin):
 
     """Custom User Admin"""
+
+    inlines = (RoomInline,)
 
     fieldsets = UserAdmin.fieldsets + (
         (
@@ -23,4 +31,19 @@ class CustomUserAdmin(UserAdmin):
                 )
             },
         ),
+    )
+
+    list_filter = UserAdmin.list_filter + ("superhost",)
+
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "language",
+        "currency",
+        "superhost",
+        "is_staff",
+        "is_superuser",
     )
