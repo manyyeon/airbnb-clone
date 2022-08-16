097907ce13c812a492ad9811b975cf6014a8e3e1
manyyeon <udayeon@naver.com>
2022-08-16 13:21:24 +0900

    Refactor : Class Based Views part two

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.views.generic import ListView
 from . import models
 
@@ -7,5 +8,13 @@ class HomeView(ListView):
     """HomeView Definition"""
 
     model = models.Room
-    paginate_orphans = paginate_by = 10
+    paginate_by = 10
+    paginate_orphans = 5
     ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
